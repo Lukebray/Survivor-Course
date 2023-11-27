ec018150e39ce726dbaf5a73b75743ad3055fc48
Lukebray <39488446+Lukebray@users.noreply.github.com>
2023-11-27 21:02:40 +0000

    added level up and upgrades

--- scenes/game_object/basic_enemy/basic_enemy.gd
+++ scenes/game_object/basic_enemy/basic_enemy.gd
@@ -20,7 +20,3 @@ func get_direction_to_player():
 	if player_node != null:
 		return (player_node.global_position - global_position).normalized()
 	return Vector2.ZERO
-	
-
-func _on_area_2d_area_entered(area):
-	health_component.damage(100)
