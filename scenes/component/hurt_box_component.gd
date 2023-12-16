719521316d46d9e2e42118a041ef0e2c45ad3216
Lukebray <39488446+Lukebray@users.noreply.github.com>
2023-12-16 18:02:16 +0000

    polish

--- scenes/component/hurt_box_component.gd
+++ scenes/component/hurt_box_component.gd
@@ -3,7 +3,7 @@ class_name HurtboxComponent
 
 @export var health_component : Node
 
-
+var float_text_scene = preload("res://scenes/ui/floating_text.tscn")
 
 func _on_area_entered(area):
 	if not area is HitboxComponent:
@@ -14,3 +14,9 @@ func _on_area_entered(area):
 	
 	var hitbox_component = area as HitboxComponent
 	health_component.damage(hitbox_component.damage)
+	
+	var floating_text = float_text_scene.instantiate() as Node2D
+	get_tree().get_first_node_in_group("foreground_layer").add_child(floating_text)
+	
+	floating_text.global_position = global_position + (Vector2.UP * 16)
+	floating_text.start(str(hitbox_component.damage))
